fa09e772c06231e0bb58d6f45f7422803119b383
SUHWA0904 <suhwa0904@gmail.com>
2023-03-06 14:55:41 +0900

    백준 1000번

---
+++ BeakJoon/src/com/psh/baekjoon/P_1000.java
@@ -0,0 +1,18 @@
+package com.psh.baekjoon;
+
+import java.util.Scanner;
+
+public class P_1000 {
+
+	public static void main(String[] args) {
+
+		Scanner sc= new Scanner(System.in);
+		int su=sc.nextInt();
+		int su2=sc.nextInt();
+		int result=su+su2;
+		System.out.println(result);
+		
+
+	}
+
+}
